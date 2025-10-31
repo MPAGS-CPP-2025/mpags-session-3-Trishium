d0c8895df3cf33eb04bf751af8cd6591195961d7
trishium <pdhumphris@gmail.com>
2025-10-31 13:02:07 +0000

    remove unneeded lines

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,64 +1,37 @@
 #include <iostream>
 #include <string>
 
-void print(const std::string string){
+void print(const std::string string)
+{
     std::cout << string << std::endl;
 }
 
-void print(const char* string){
+void print(const char* string)
+{
     std::cout << string << std::endl;
 }
 
-void print(const char string){
+void print(const char string)
+{
     std::cout << string << std::endl;
 }
 
-int main(){
-    // comment
-    /*
-     comment
-    */
-    std::string hello{"Hello World!"};
-    int a{2};
-    a = 3;
-    a++;
-    a = a%3;
-    a = a << 2;
-    const double b{1.2};
-
-    const int d[4]{1,2,3,4};
-    const int e{d[0]+d[1]+d[2]+d[3]};
-    std::cout << a << " is an integer" << std::endl;
-    std::cout << b << " is a double" << std::endl;
-    const double c{a*b};
-    std::cout << c << std::endl;
-    std::cout << hello << std::endl;
-    std::cout << d[0] << " is the start of a list" << std::endl;
-    std::cout << e << " is the sum of digits 1 to 4" << std::endl;
-
-    std::cout << a/b << " An int divided by a double" << std::endl;
-    std::cout << b/a << " A double divided by an int" << std::endl;
-    std::cout << e/a << " An int divided by an int" << std::endl;
-    
-    const char letter{hello[0]};
-    print(std::string{letter} + std::string{" is a letter in the hello variable"});
-
-    print("prep all done!");
-
+int main()
+{
     char in_char{'x'};
     std::string out_str{""};
 
-    while(std::cin >> in_char){
-        if (in_char == '#'){
+    while (std::cin >> in_char) {
+        if (in_char == '#') {
             print("exiting");
             break;
         }
-        if(std::isalpha(in_char)){
+        if (std::isalpha(in_char)) {
             in_char = std::toupper(in_char);
             out_str += in_char;
-        }
-        else{
-            switch (in_char){
+        } 
+        else {
+            switch (in_char) {
                 case '0':
                     out_str += "ZERO";
                     break;
@@ -93,9 +66,7 @@ int main(){
                     print("Characters must be alphanumeric");
                     break;
             }
-            
         }
-
     }
     print(out_str);
     return 0;
